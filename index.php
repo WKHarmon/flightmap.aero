0a7525f332c55561eb401adf897e656cf8a795ba
WKHarmon <kyle@kyleharmon.com>
2016-01-18 16:26:51 -0800

    Require SSL for connections.

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+if(!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == ""){
+    $redirect = "https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+    header("HTTP/1.1 301 Moved Permanently");
+    header("Location: $redirect");
+}
 require_once('includes/functions.php');
 $flights = new flights();
 ?>
